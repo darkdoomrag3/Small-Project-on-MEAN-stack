bd32f72f1b7e8d0bdf3ceb66290e6ab20e26881f
darkdoomrag3 <47889899+darkdoomrag3@users.noreply.github.com>
2020-09-01 17:22:41 +0430

    adding some routes and setup basic router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
-import { Post } from './posts/post.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  stpredPosts: Post[] = [];
-  onPostAdded(post) {
-    this.stpredPosts.push(post);
-  }
+
 }
--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,22 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
-import { Post } from '../post.model';
-import { NgForm } from '@angular/forms';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { PostsService } from "../posts.service";
 
 @Component({
-  selector: 'app-post-create',
-  templateUrl: './post-create.component.html',
-  styleUrls: ['./post-create.component.css'],
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
 })
 export class PostCreateComponent {
-  enteredTitle: '';
-  enteredContent: '';
-  @Output() postCreate = new EventEmitter<Post>();
+  enteredTitle = "";
+  enteredContent = "";
+
+  constructor(public postsService: PostsService) {}
 
   onAddPost(form: NgForm) {
-    const post: Post = {
-      title: form.value.title,
-      content: form.value.content,
-    };
-    this.postCreate.emit();
+    if (form.invalid) {
+      return;
+    }
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
